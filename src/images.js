e10aaa386b78f379e62afda93db989bca9fbaf4a
Arif Furqon <arif.furqon@gmail.com>
2019-08-07 00:27:15 -0700

    added images file

---
+++ src/images.js
@@ -0,0 +1,22 @@
+export const images = [
+	{
+		url: "https://images.unsplash.com/photo-1524704654690-b56c05c78a00?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80",
+		caption: "Anthias are pretty fish which school in large numbers over tropical coral reefs."
+	},
+	{
+		url: "https://images.unsplash.com/photo-1522720833375-9c27ffb02a5e?ixlib=rb-1.2.1&auto=format&fit=crop&w=2574&q=80",
+		caption: "Angelfishes are some of the most beautiful and colourful fishes found on coral reefs."
+	},
+	{
+		url: "https://images.unsplash.com/photo-1524704796725-9fc3044a58b2?ixlib=rb-1.2.1&auto=format&fit=crop&w=2581&q=80",
+		caption: "A gorgeous surgeon fish or tang swims past at the Cairns aquarium."
+	},
+	{
+		url: "https://images.unsplash.com/photo-1516408388733-2f8364f2e00b?ixlib=rb-1.2.1&auto=format&fit=crop&w=2502&q=80",
+		caption: "A beautiful butterfly fish that lives on coral reefs. Also called a Beaked Coralfish."
+	},
+	{
+		url: "https://images.unsplash.com/photo-1520990056956-5d29b4884100?ixlib=rb-1.2.1&auto=format&fit=crop&w=2518&q=80",
+		caption: "A spot-fin Lionfish in all its glory."
+	},
+]
